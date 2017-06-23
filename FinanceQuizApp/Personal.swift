e2ab9256571c929d9f1d04ff9e8d09e5251950e4
D0515211 <d0515211@iMac01.local>
2017-06-23 23:42:43 +0800

    Change Interface

---
+++ FinanceQuizApp/Personal.swift
@@ -0,0 +1,20 @@
+//
+//  Personal.swift
+//  FinanceQuizApp
+//
+//  Created by D0515211 on 2017/6/23.
+//  Copyright © 2017年 訪客使用者. All rights reserved.
+//
+
+import Foundation
+
+class Account{
+    var id : String
+    var job : String
+    
+    init(id:String, job : String = "") {
+        self.id = id
+        self.job = job
+    }
+    
+}
